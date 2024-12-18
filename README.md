e30a7b72e4d937dca4225dc7038ee321dc6d455f
AlexanderBuzz <102560752+AlexanderBuzz@users.noreply.github.com>
2024-12-18 19:51:13 +0100

    Added the fun part

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # XRPL-Ruby [WIP]
+
 A Ruby library to interact with the XRP Ledger (XRPL) blockchain
+
+![The Work](the_work.jpg)
--- example/codec.rb
+++ example/codec.rb
@@ -1,18 +1,18 @@
 require "xrpl-ruby"
 
-codec = AddressCodec::AddressCodec.new
+address_codec = AddressCodec::AddressCodec.new
 
-# encoded = codec.encode_raw("test".bytes)
+# input = "test"
+# puts "Input String: #{test}"
+# encoded = codec.encode_raw(test.bytes)
 # puts "Encoded: #{encoded}"
-
 # decoded = codec.decode_raw(encoded)
 # puts "Decoded: #{decoded}"
 
 # encoded = codec.encode_seed(hex_to_bytes("4C3A1D213FBDFB14C7C28D609469B341"))
 # puts "Encoded: #{encoded}"
 
-
-puts "TEST"
 classic_address = "r9cZA1mLK5R5Am25ArfXFmqgNwjZgnfk59"
-x_address = codec.classic_address_to_x_address(classic_address, 0, false)
+puts "Classic Address: #{classic_address}"
+x_address = address_codec.classic_address_to_x_address(classic_address, 0, false)
 puts "xAddress: #{x_address}"
